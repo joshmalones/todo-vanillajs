29b95ef2d16aee388874373ad71a5a0875f881bb
Josh <joshbaon1@gmail.com>
2020-11-11 17:56:26 -0800

    adding functions

--- app.js
+++ app.js
@@ -2,10 +2,30 @@
 
 const todoInput = document.querySelector(".todoList__input");
 const todoButton = document.querySelector(".todoList__button");
-const TodoList = document.querySelector(".todoList__list");
+const todoList = document.querySelector(".todoList");
 
 // Event Listener
 todoButton.addEventListener("click", (event) => {
-  //Prevent from loading
+  // Prevent from loading
   event.preventDefault();
+  // Todo Div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  // Create Li
+  const newTodo = document.createElement("li");
+  newTodo.innerText = "Todo";
+  newTodo.classList.add("todo__item");
+  todoDiv.appendChild(newTodo);
+  // Check mark button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("completed__btn");
+  todoDiv.appendChild(completedButton);
+  // deleted button
+  const deleteButton = document.createElement("button");
+  deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+  deleteButton.classList.add("delete__btn");
+  todoDiv.appendChild(deleteButton);
+  // Append to list
+  todoList.appendChild(todoDiv);
 });
